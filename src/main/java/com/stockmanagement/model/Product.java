b3f1c8ae98047fea1fbfee480d3dbdc13b5f3f01
Jayasinghe D.B.P (IT24100191) <IT24100191@my.sliit.lk>
2025-05-13 09:47:37 +0530

    make Product serializable

--- src/main/java/com/stockmanagement/model/Product.java
+++ src/main/java/com/stockmanagement/model/Product.java
@@ -1,6 +1,10 @@
 package com.stockmanagement.model;
 
-public class Product extends AbstractEntity {
+import java.io.Serializable;
+
+public class Product extends AbstractEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private String category;
     private double price;
