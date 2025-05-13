9f8d6d5616a743ef3172f49585f6cbb681bbd4a9
Jayasinghe D.B.P (IT24100191) <IT24100191@my.sliit.lk>
2025-05-13 09:50:19 +0530

    implement ProductStack.java using stack data structure

--- src/main/java/com/stockmanagement/util/ProductStack.java
+++ src/main/java/com/stockmanagement/util/ProductStack.java
@@ -1,4 +1,21 @@
 package com.stockmanagement.util;
 
+import com.stockmanagement.model.Product;
+
+import java.util.Stack;
+
 public class ProductStack {
+    private final Stack<Product> stack = new Stack<Product>();
+
+    public void push(Product product) {
+        stack.push(product);
+    }
+
+    public Product pop() {
+        return stack.isEmpty() ? null : stack.pop();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
 }
