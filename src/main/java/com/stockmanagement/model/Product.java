fd0b84c5105cfb20f18f22116dc1d148b80a1c83
Jayasinghe D.B.P (IT24100191) <IT24100191@my.sliit.lk>
2025-05-13 09:29:20 +0530

    modify Product.java

--- src/main/java/com/stockmanagement/model/Product.java
+++ src/main/java/com/stockmanagement/model/Product.java
@@ -1,27 +1,19 @@
 package com.stockmanagement.model;
 
-public class Product {
-    protected int id;
-    protected String name;
-    protected double price;
-    protected int quantity;
+public class Product extends AbstractEntity {
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+    private int stockAlertLimit;
 
-
-    public Product() {
-    }
-    public Product(int id, String name, double price, int quantity) {
-        this.id = id;
+    public Product(String id, String name, String category, double price, int quantity, int stockAlertLimit) {
+        super(id);
         this.name = name;
+        this.category = category;
         this.price = price;
         this.quantity = quantity;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+        this.stockAlertLimit = stockAlertLimit;
     }
 
     public String getName() {
@@ -30,6 +22,14 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public double getPrice() {
@@ -47,18 +47,29 @@ public class Product {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-    public static Product fromString(String line) {
-        String[] parts = line.split(",");
-        return new Product(
-                Integer.parseInt(parts[0]),
-                parts[1],
-                Double.parseDouble(parts[2]),
-                Integer.parseInt(parts[3])
-        );
-    }
-    @Override
-    public String toString() {
-        return id + "," + name + "," + price + "," + quantity;
+
+    public int getStockAlertLimit() {
+        return stockAlertLimit;
     }
 
+    public void setStockAlertLimit(int stockAlertLimit) {
+        this.stockAlertLimit = stockAlertLimit;
+    }
+
+    @Override
+    public String toFileString() {
+        return id + "," + name + "," + category + "," + price + "," + quantity + "," + stockAlertLimit;
+    }
+
+    public static Product fromFileString(String fileString) {
+        String[] parts = fileString.split(",");
+        return new Product(
+                parts[0],
+                parts[1],
+                parts[2],
+                Double.parseDouble(parts[3]),
+                Integer.parseInt(parts[4]),
+                Integer.parseInt(parts[5])
+        );
+    }
 }
