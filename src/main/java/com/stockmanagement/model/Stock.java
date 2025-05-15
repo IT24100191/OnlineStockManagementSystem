0b562eefd73c8e8cf3b43870d784efdd53c37f6f
IT24100389 <it24100389@my.sliit.lk>
2025-05-15 22:29:12 +0530

    Add Stock model class

---
+++ src/main/java/com/stockmanagement/model/Stock.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.time.LocalDate;
+
+
+public class Stock {
+    private String id;
+    private String name;
+    private String category;
+    private int quantity;
+    private String unit;
+    private LocalDate expiryDate;
+
+    public Stock(String id, String name, String category, int quantity, String unit, LocalDate expiryDate) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public int getQuantity() { return quantity; }
+    public String getUnit() { return unit; }
+    public LocalDate getExpiryDate() { return expiryDate; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+
+}
